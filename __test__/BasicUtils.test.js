8ee511101b3891df44f907fef42d5dd59f56d1e8
kemboi590 <bkemboi590@gmail.com>
2025-05-21 00:23:32 +0300

    updated my tsconfig to not transpile my tests

--- __test__/BasicUtils.test.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const BasicUtils_1 = require("../src/BasicUtils");
-describe("BasicUtils test suite ", () => {
-    it("should return the product of 3 and 2 ", () => {
-        const actual = (0, BasicUtils_1.product)(3, 2);
-        expect(actual).toBe(6);
-    });
-});
